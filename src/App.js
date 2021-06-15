7be3bd0eb133c7ba4c3dac865a8da481b4103974
Hackyeahh <henry.xiu@gx.cds.on.ca>
2021-06-15 16:02:20 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <center><h1 className='centered'>Prometheus.</h1></center>
+
+      <Pages.Home />
+    </>
   );
 }
+
+class Pages{
+  static Home(){
+    return (
+      <Sections.Landing />
+    );
+  }
+
+  // static About(){
+  //   return (
+  //     // how it gets rendered
+  //   );
+  // } 
+}
+
+class Sections {
+  static Landing() {
+    return (
+      <MenuBar />
+    );
+  }
+}
+
+function MenuBar (){
+  return (
+    <div className='nav-bar centered row'>
+      <a className='nav'>Home</a> 
+      <a className='nav'>About Us</a>
+      <a className='nav'>Projects</a>
+      <a className='nav'>Articles</a>
+      <a className='nav'>Editorials</a>
+    </div>
+
+  );
+}
+
+// function Person({age, gender}){
+
+//   console.log("test");
+
+//   return (
+//     <h1 className='ecolor yellow-text'>I am {age} years old {gender}.</h1>
+//   );
+// }
+
 
 export default App;
