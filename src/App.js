cafc77fb3b4054166032004c51ef679edadbf202
Indom1nus <arjun.walia@gx.cds.on.ca>
2021-06-23 15:50:55 -0400

    Second Day
    Co-Authored-By: yvngadumkid <83147436+yvngadumkid@users.noreply.github.com>
    Co-Authored-By: Marianna <83137694+MariannaSperanza@users.noreply.github.com>
    Co-Authored-By: TheTrueRoy <66959950+TheTrueRoy@users.noreply.github.com>
    Co-Authored-By: Hackyeahh <66337946+Hackyeahh@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './styles.scss';
 import ParticlesContainer from './particles.js';
+import { IconContext } from "react-icons";
+import { FontAwesome } from "react-icons/fa";
+import { FaTwitter, FaGithub, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 
 // figma design https://www.figma.com/file/24mDiMckplH7BUcGwQfnJW/ProjectPrometheus
 // 
+
 
 
 function App() {
@@ -31,21 +36,38 @@ class Pages{
 class Sections {
   static Landing() {
     return (
-      <ParticlesContainer children={
-        <div className='f1'>
-          <NavBar />
-          <Name />
-        </div>
-      }/>
-      
+      <section>
+        <ParticlesContainer children={
+          <div>
+            <NavBar />
+            <div className='row'>
+              <Name />
+              <SocialMediaIconsLanding />
+            </div>
+          </div>
+          
+        }/>
+      </section>
+
+
     );
   }
+
+  static Statistic(){
+    return (
+      <h1>
+        <span></span>
+      </h1>
+    );
+  }
+
 }
+
 
 function NavBar (){
   return (
     
-    <div className='navbar row center'>
+    <div className='navbar row center f1'>
       <a class='navbar'>Home</a> 
       <a class='navbar'>About Us</a>
       <a class='navbar'>Projects</a>
@@ -60,13 +82,35 @@ function NavBar (){
 
 function Name (){
   return (
-    <div className='center'>
-      <div className='name row center'>
-        <h2 className='name vertical-text'>project</h2>
-        <h1 className='name'>prometheus</h1>
-      </div>
+    <section className='name'>
+        <div className='name row center f1'>
+          <h2 className='name vertical-text'>project</h2>
+          <h1 className='name'>prometheus</h1>
+          
+        </div>
+        <h3 className = 'name f2'>Ignite the mind. Pass on the torch.</h3>
+    </section>
+  );
+}
 
-    </div>
+
+function SocialMediaIconsLanding(){
+  // const iconStyles = {
+  //   color: '#FF9400',
+  //   fontSize: '500px',
+  //   height: '500px',
+  //   width: '500px',
+  // }
+
+  return (
+    
+        <div className='icon-landing column'>
+          <FaInstagram className='icon'/>
+          <FaTwitter className='icon'/>
+          <FaGithub className='icon'/>
+          <FaLinkedin className='icon'/>
+        </div>
+    
   );
 }
 
