db6dfe5bd39aa9678213d7610e5a7132f8eb7756
Hackyeahh <henry.xiu@gx.cds.on.ca>
2021-08-08 14:11:37 -0400

    Cards
    - stole cards
    
    Co-Authored-By: Indom1nus <66338057+Indom1nus@users.noreply.github.com>
    Co-Authored-By: Marianna <83137694+MariannaSperanza@users.noreply.github.com>
    Co-Authored-By: TheTrueRoy <66959950+TheTrueRoy@users.noreply.github.com>
    Co-Authored-By: yvngadumkid <83147436+yvngadumkid@users.noreply.github.com>

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -74,16 +74,46 @@ function OurMission(){
 
 
 function Initiatives(){
+
+  const articles = {
+    heading: "Articles",
+    subheading: "Lorem ipsum dolor sit amet.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  }
+
+  const editorials = {
+    heading: "Editorials",
+    subheading: "Lorem ipsum dolor sit amet.",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+  }
+
   return(
-    <section className='initiatives'>
+    <section className='initiatives row center-main'>
+      <Card 
+        heading={articles.heading}
+        subheading={articles.subheading}
+        description={articles.description}
+        href='/about'
+      />
 
+      <Card 
+        heading={editorials.heading}
+        subheading={editorials.subheading}
+        description={editorials.description}
+      />
 
+      
+
+{/* 
       <div className='initiatives-grid'>
         <Tile title='Articles' text='bingus, arjun is a cool guy' background='#FF9400'/>
         <Tile title='Editorials' background='#000000'/>
         <Tile title='CDS Tech Club'  background='#008000'/>
         <Tile title='CCC' defaultText='test1' hoverText='please work' background='#FF0000'/>
-      </div>
+      </div> */}
+
+
+      
     </section>
   )
 }
@@ -110,53 +140,51 @@ function Name(){
 function SocialMediaIconsLanding(){
   return (
     <div className='center-main icon-landing row moving2'>
-      <div  style={{justifyContent: 'center'}}>
         <FaInstagram className='icon'/>
         <FaTwitter className='icon'/>
         <FaGithub className='icon'/>
         <FaLinkedin className='icon'/>
-      </div>
     </div>
   );
 }
 
 
 
-function Tile({ title, defaultText, hoverText, image, onClick, background }) { // must be a part of a grid
-  const style = {
-    backgroundImage: image,
-    gridArea: title.toLowerCase().replace(/\s/g, ''), 
-    backgroundColor: background,
-  }
+// function Tile({ title, defaultText, hoverText, image, onClick, background }) { // must be a part of a grid
+//   const style = {
+//     backgroundImage: image,
+//     gridArea: title.toLowerCase().replace(/\s/g, ''), 
+//     backgroundColor: background,
+//   }
 
-  const [displayedText, setDisplayedText] = useState(defaultText); //properties that change THAT NEED TO BE RENDERED ALSO, defaultText is the initial state, useState returns a set of values, displayedText and setDisplayedText have a value of defaultText
+//   const [displayedText, setDisplayedText] = useState(defaultText); //properties that change THAT NEED TO BE RENDERED ALSO, defaultText is the initial state, useState returns a set of values, displayedText and setDisplayedText have a value of defaultText
 
-  const [textHoveringClass, setHoveringClass] = useState(false); //ones a setter one's a getter
+//   const [textHoveringClass, setHoveringClass] = useState(false); //ones a setter one's a getter
 
-  const onMouseEnter = () => {
-    setHoveringClass('tile-text-hover');
-    setDisplayedText(hoverText);
-  }
+//   const onMouseEnter = () => {
+//     setHoveringClass('tile-text-hover');
+//     setDisplayedText(hoverText);
+//   }
 
-  const onMouseExit = () => {
-    setHoveringClass('tile-text-normal');
-    setDisplayedText(defaultText);
-  }  
+//   const onMouseExit = () => {
+//     setHoveringClass('tile-text-normal');
+//     setDisplayedText(defaultText);
+//   }  
 
   
 
-  return( // inline style
-    <div className='tile column' onClick={onClick} style={style} onMouseEnter={onMouseEnter} onMouseLeave={onMouseExit}>
-      <div className='tile-content'>
-        <h1 className='f1 tile-text'>{title}</h1>
-        <Line color='white' opacity={0.5} width='40%' />
-        <p className={`f2 tile-text ${textHoveringClass}`}>{displayedText}</p>
-      </div>
+//   return( // inline style
+//     <div className='tile column' onClick={onClick} style={style} onMouseEnter={onMouseEnter} onMouseLeave={onMouseExit}>
+//       <div className='tile-content'>
+//         <h1 className='f1 tile-text'>{title}</h1>
+//         <Line color='white' opacity={0.5} width='40%' />
+//         <p className={`f2 tile-text ${textHoveringClass}`}>{displayedText}</p>
+//       </div>
 
-    </div>
-  );
+//     </div>
+//   );
 
-}
+// }
 
 function TestingThing(){
   const style = {
@@ -171,5 +199,26 @@ function TestingThing(){
   )
 }
 
+
+function Card({ heading, subheading, description, href='#'}){
+
+  
+
+  return (
+    <div>
+      <a className='card' href={href}>
+        <h3 className='f1 card'>{heading}</h3>
+        <h4 className='f2 card'>{subheading}</h4>
+        <p className='f2 card'>{description}</p>
+        <span class="link-text f2">
+          View our {heading.toLowerCase()}
+            <svg width="25" height="16" viewBox="0 0 25 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path fill-rule="evenodd" clip-rule="evenodd" d="M17.8631 0.929124L24.2271 7.29308C24.6176 7.68361 24.6176 8.31677 24.2271 8.7073L17.8631 15.0713C17.4726 15.4618 16.8394 15.4618 16.4489 15.0713C16.0584 14.6807 16.0584 14.0476 16.4489 13.657L21.1058 9.00019H0.47998V7.00019H21.1058L16.4489 2.34334C16.0584 1.95281 16.0584 1.31965 16.4489 0.929124C16.8394 0.538599 17.4726 0.538599 17.8631 0.929124Z" fill="#753BBD"/>
+            </svg>
+        </span>
+      </a>
+    </div>
+  );
+}
 
 export default HomePage;
