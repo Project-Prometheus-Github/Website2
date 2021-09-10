ca4d9172dc21e5cecb7e91c625d70e953bb7e147
Hackyeahh <66337946+Hackyeahh@users.noreply.github.com>
2021-09-10 15:14:13 -0400

    BIG MONEY
    LETS GOOO I GOT IT WORKING
    
    Co-Authored-By: Indom1nus <66338057+Indom1nus@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import "./global/global.scss";
 import HomePage from "./pages/home/home.js"
 import AboutPage from "./pages/about/about.js"
+import ProjectsPage from "./pages/projects/projects.js"
 
 // figma design https://www.figma.com/file/24mDiMckplH7BUcGwQfnJW/ProjectPrometheus
 // 
@@ -22,6 +23,10 @@ function App() {
 
         <Route path='/about'>
           <AboutPage />
+        </Route>
+
+        <Route path='/projects'>
+          <ProjectsPage />
         </Route>
 
       </Switch>
--- src/pages/projects/projects.js
+++ src/pages/projects/projects.js
@@ -18,33 +18,45 @@ import Line from '../../global/components/line.js';
 import NavBar from '../../global/components/navbar';
 import Footer from '../../global/components/footer';
 
+const pic = 'https://media.istockphoto.com/photos/always-brainstorming-picture-id1184334685?k=20&m=1184334685&s=612x612&w=0&h=K5ADo3hUhIXJnQUTymQM1J1aq5syuy3kL7yDbjs_tg4='
+
 function ProjectsPage(){
     return (
-        <Event />
+        <>
+                <Event right heading='title text' description='diwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokd' src={pic}/>
+        
+        <Event heading='title text' description='diwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokddiwaidawijiejwandwjandwoadwaodawokd' src={pic}/>
+        </>
+
     );
+
 }
 
 function Event({ right, heading, description, src }){
-    // by default text is on the left, but if the right prop is passed, then the text and image will be switched.'
+    // by default text is on the right, but if the right prop is passed, then the text and image will be switched.'
 
-    const TextComponent = (heading, description,) => {
+    const TextComponent = ({heading, description,}) => {
         return (
-            <div>
+            <div className='event-component-text'>
                 <h1 className='f2'>{heading}</h1>
-                <Line height={2} width='40%'/>
+                <Line height={2} width='40%' color='black'/>
                 <p>{description}</p>
             </div>
         );
     }
 
+
+
     return (
-        <section>
-            <div>
-                {right ? null : <TextComponent heading={heading} description={description}/>}
-                <img src={PromLogo2}/>
-                {right ? <TextComponent heading={heading} description={description}/> : null}
+        <section className='event-component-parent'>
+            <div className='event-component'>
+                {right ? null : <img className='event-component-image' src={src}/> }
+                <TextComponent heading={heading} description={description}/>
+                {right ? <img className='event-component-image' src={src}/> : null }
             </div>
 
         </section>
     );
 }
+
+export default ProjectsPage;
